875dbce1bc7fb0845f642d5d194473045beaf024
alexpdk <alexeipodkorytov65@gmail.com>
2016-10-03 18:18:52 +0500

    Sandbox app to study ORB opportunities

--- app/src/androidTest/java/com/weiaett/cruelalarm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/weiaett/cruelalarm/ExampleInstrumentedTest.java
@@ -1,8 +1,11 @@
 package com.weiaett.cruelalarm;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- app/src/test/java/com/weiaett/cruelalarm/ExampleUnitTest.java
+++ app/src/test/java/com/weiaett/cruelalarm/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.weiaett.cruelalarm;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
