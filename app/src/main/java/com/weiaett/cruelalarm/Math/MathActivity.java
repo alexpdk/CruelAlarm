21405f8507af92d275cf3d953e5e1f761cc774ba
Denis Kibel <dekibel@yandex.ru>
2016-11-15 13:53:12 +0600

    Add basic math expressions support

---
+++ app/src/main/java/com/weiaett/cruelalarm/Math/MathActivity.java
@@ -0,0 +1,100 @@
+package com.weiaett.cruelalarm.Math;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.weiaett.cruelalarm.R;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import kotlin.TuplesKt;
+
+public class MathActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MathActivityAdapter mathActivityAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math);
+
+
+        TextView tvExpression = (TextView) findViewById(R.id.tvExpression);
+        String[] expression = makeExpression();
+        tvExpression.setText(expression[0] + " = ?");
+
+        recyclerView = (RecyclerView) this.findViewById(R.id.grid);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        String[] answers = new String[10];
+        for (int i = 0; i < 10; i++) {
+            answers[i] = expression[i + 1];
+        }
+        mathActivityAdapter = new MathActivityAdapter(expression[0], answers, recyclerView);
+        recyclerView.setAdapter(mathActivityAdapter);
+
+        //  generate expression DONE
+        //  calculate result DONE
+        //  create array DONE
+        //  Для ответов посмотреть фотомоменджер. recycler-layout (grid)
+        //  Создать адаптер как в фотоменеджере или алармлист
+        //  Поместить в отдельную папку
+    }
+
+    //  Возвращает выражение и ответ к нему
+    private String[] makeExpression() {
+        Random rand = new Random(System.currentTimeMillis());
+        char operands[] = {'*', '/', '-', '+'};
+        String[] output = new String[11];
+
+        int firstNumber = rand.nextInt(500) + 300;
+        int answer = firstNumber;
+        output[0] = Integer.toString(firstNumber);
+
+        char firstOperand = operands[rand.nextInt(2) + 2];
+        output[0] += " " + Character.toString(firstOperand) + " ";
+
+        char secondOperand = operands[rand.nextInt(2)];
+        int secondNumber,
+            thirdNumber = rand.nextInt(10) + 20;
+        if (secondOperand == '/') {
+            int tmp = rand.nextInt(9) + 1;
+            if (firstOperand == '+')
+                answer += tmp;
+            else
+                answer -= tmp;
+            secondNumber = tmp * thirdNumber;
+        } else {
+            secondNumber = rand.nextInt(9) + 1;
+            int mult = secondNumber * thirdNumber;
+            if (firstOperand == '+')
+                answer += mult;
+            else
+                answer -= mult;
+        }
+
+        output[0] += Integer.toString(secondNumber)
+                  + ' ' + secondOperand + ' ' + thirdNumber;
+        output[1] = Integer.toString(answer);
+
+        HashSet<Integer> answers = new HashSet<>();
+        answers.add(answer);
+        while (answers.size() != 10)
+            answers.add(rand.nextInt(1000));
+        answers.remove(answer);
+
+        int i = 2;
+        for (int ans : answers) {
+            output[i++] = Integer.toString(ans);
+        }
+        return output;
+    }
+}
---
+++ app/src/main/java/com/weiaett/cruelalarm/Math/MathActivityAdapter.java
@@ -0,0 +1,91 @@
+package com.weiaett.cruelalarm.Math;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.weiaett.cruelalarm.R;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by dekib_000 on 12.11.2016.
+ */
+
+public class MathActivityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private RecyclerView recyclerView;
+    private String expression;
+    private String correctAnswer;
+    private String[] answers;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mathAnswer;
+
+        public ViewHolder(View v) {
+            super(v);
+            mathAnswer = (TextView) v.findViewById(R.id.mathAnswer);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.mathAnswer.setText(answers[position]);
+        viewHolder.mathAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (viewHolder
+                        .mathAnswer
+                        .getText()
+                        .toString()
+                        .equals(correctAnswer)) {
+                    viewHolder
+                            .mathAnswer
+                            .setText("Crrect");
+                } else {
+                    viewHolder
+                            .mathAnswer
+                            .setText("Wrong");
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return answers.length;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View myView = LayoutInflater.from(context)
+                .inflate(R.layout.card_math, parent, false);
+        return new ViewHolder(myView);
+    }
+
+    MathActivityAdapter(String expression, String[] answers, RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+        this.expression = expression;
+        this.answers = answers;
+        this.correctAnswer = answers[0];
+        shuffleArray(this.answers);
+    }
+
+    private void shuffleArray(String[] arr) {
+        Random rand = new Random(System.currentTimeMillis());
+        for (int i = 0; i < arr.length; i++) {
+            int index = rand.nextInt(arr.length);
+            String tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp;
+        }
+    }
+}
